5a81f0ea2d4cc106fecb80c55edb235fc2106ada
Darren <darren@macbookpro.inspiringfamily.com>
2025-06-07 13:37:43 -0700

    Changed the way reset functions for game settings - allows visually to see what reset does before closing the dialog Changed the size of all the player piece color selection - makes it look and fit 'nicer' with surrounding Ui elements

--- cgamesettings.cpp
+++ cgamesettings.cpp
@@ -23,7 +23,6 @@ CGameSettings::CGameSettings(QWidget *parent)
     ui->setupUi(this);
 
     // setup the Board Color control, including having a mouse click / touch filter
-    ui->ctlBoardColor->setStyleSheet(QString("background-color: %1").arg(pSettings->boardColor().name()));
     CEventFilter *clickFilter = new CEventFilter(ui->ctlBoardColor);
     ui->ctlBoardColor->installEventFilter(clickFilter);
 
@@ -33,17 +32,10 @@ CGameSettings::CGameSettings(QWidget *parent)
         QColor color = QColorDialog::getColor(Qt::white, nullptr, "Select Color");
 
         if(color.isValid()) {
-            qDebug() << "selected color: " << color;
-            QString colorName = color.name();
-            QString backgroundStyle = QString("background-color: %1;").arg(colorName);
-            ui->ctlBoardColor->setStyleSheet(backgroundStyle);
             pSettings->setBoardColor(color);
+            setCtlsToGameSettings();
         }
     });
-
-    // setup the Player Color control
-    QPixmap firstPlayerIcon = QPixmap::fromImage(createColoredSvgButton(":/images/glassbutton", pSettings->playerColor(eColor::White), QSize(40,40)));
-    ui->btnFirstPlayerColor->setIcon(QIcon(firstPlayerIcon));
 
     // Connect signal to a slot
     ui->btnFirstPlayerColor->connect(ui->btnFirstPlayerColor, &QPushButton::clicked, [this,pSettings]() {
@@ -51,16 +43,10 @@ CGameSettings::CGameSettings(QWidget *parent)
         QColor color = QColorDialog::getColor(Qt::white, nullptr, "Select Color");
 
         if(color.isValid()) {
-            qDebug() << "selected color: " << color;
-            QPixmap firstPlayerIcon = QPixmap::fromImage(createColoredSvgButton(":/images/glassbutton", color, QSize(40,40)));
-            ui->btnFirstPlayerColor->setIcon(QIcon(firstPlayerIcon));
             pSettings->setPlayerColor(eColor::White, color);
+            setCtlsToGameSettings();
         }
     });
-
-    // setup the Player Color control
-    QPixmap secondPlayerIcon = QPixmap::fromImage(createColoredSvgButton(":/images/glassbutton", pSettings->playerColor(eColor::Black), QSize(40,40)));
-    ui->btnSecondPlayerColor->setIcon(QIcon(secondPlayerIcon));
 
     // Connect signal to a slot
     ui->btnSecondPlayerColor->connect(ui->btnSecondPlayerColor, &QPushButton::clicked, [this,pSettings]() {
@@ -68,17 +54,16 @@ CGameSettings::CGameSettings(QWidget *parent)
         QColor color = QColorDialog::getColor(Qt::white, nullptr, "Select Color");
 
         if(color.isValid()) {
-            qDebug() << "selected color: " << color;
-            QPixmap secondPlayerIcon = QPixmap::fromImage(createColoredSvgButton(":/images/glassbutton", color, QSize(40,40)));
-            ui->btnSecondPlayerColor->setIcon(QIcon(secondPlayerIcon));
             pSettings->setPlayerColor(eColor::Black, color);
+            setCtlsToGameSettings();
         }
     });
 
     ui->btnDialogBox->connect(ui->btnDialogBox->button(QDialogButtonBox::Reset), &QPushButton::clicked, [this,pSettings]()
     {
         pSettings->resetSettings();
-        this->accept();
+        setCtlsToGameSettings();
+
     });
 
     ui->btnDialogBox->connect(ui->btnDialogBox->button(QDialogButtonBox::Close), &QPushButton::clicked, [this](){applyChanges();});
@@ -87,17 +72,21 @@ CGameSettings::CGameSettings(QWidget *parent)
 
 
 void CGameSettings::showEvent(QShowEvent *event) {
+    setCtlsToGameSettings();
+}
+
+void CGameSettings::setCtlsToGameSettings(){
     CSettings* pSettings = CSettings::getGlobalInstance();
 
     // setup the Board Color control, including having a mouse click / touch filter
     ui->ctlBoardColor->setStyleSheet(QString("background-color: %1").arg(pSettings->boardColor().name()));
 
     // setup the Player Color control
-    QPixmap firstPlayerIcon = QPixmap::fromImage(createColoredSvgButton(":/images/glassbutton", pSettings->playerColor(eColor::White), QSize(40,40)));
+    QPixmap firstPlayerIcon = QPixmap::fromImage(createColoredSvgButton(":/images/glassbutton", pSettings->playerColor(eColor::White), QSize(36,36)));
     ui->btnFirstPlayerColor->setIcon(QIcon(firstPlayerIcon));
 
     // setup the Player Color control
-    QPixmap secondPlayerIcon = QPixmap::fromImage(createColoredSvgButton(":/images/glassbutton", pSettings->playerColor(eColor::Black), QSize(40,40)));
+    QPixmap secondPlayerIcon = QPixmap::fromImage(createColoredSvgButton(":/images/glassbutton", pSettings->playerColor(eColor::Black), QSize(36,36)));
     ui->btnSecondPlayerColor->setIcon(QIcon(secondPlayerIcon));
 }
 
